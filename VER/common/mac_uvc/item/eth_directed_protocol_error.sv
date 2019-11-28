17725732e85a9b94e34e76ab74d07f32aef30227
wanggb <wanggb@pcl.ac.cn>
2019-11-28 16:23:15 +0800

    modify code

--- VER/common/mac_uvc/item/eth_directed_protocol_error.sv
+++ VER/common/mac_uvc/item/eth_directed_protocol_error.sv
@@ -1,36 +1,3 @@
-//FILE_HEADER------------------------------------------------------------------------------------------
-// ZTE  Copyright
-// ZTE Company Confidential
-//-----------------------------------------------------------------------------------------------------
-// FILE NAME:       eth_directed_protocol_error.sv
-// DEPARTMENT:      Multi-Service Bearer Product ShenZhen Design&Development Dept.
-// AUTHOR:          Li Xiangqiong
-// AUTHOR'S EMAIL:  li.xiangqiong@zte.com.cn
-//-----------------------------------------------------------------------------------------------------
-// RELEASE HISTORY: N/A
-// VERSION      DATE         AUTHOR       DESCRIPTION
-// 1.1         2010-5-28    Li Xiangqiong   UVM
-//-----------------------------------------------------------------------------------------------------
-// KEYWORDS: eth_directed_protocol_error
-//-----------------------------------------------------------------------------------------------------
-// PURPOSE: 
-//--------------------------------------------------------------------------------------------------
-// PARAMETERS
-//         PARAM  NAME    RANGE    : DESCRIPTION    :DEFAULT   :UNITS
-
-//-----------------------------------------------------------------------------------------------------
-//REUSE ISSUES
-// Reset Strategy:  N/A
-// Clock  Domains:  N/A
-// Critical Timing: N/A
-// Test   Features: N/A
-// Asynchronous I/F:N/A
-// Scan Methodology:N/A
-// Instaniations:   N/A
-// Synthesizable:   N/A
-// Other:   
-// END_HEADER---------------------------------------------------------------------------------------------
-
 `ifndef ETH_DIRECTED_PROTOCOL_ERROR_SV
 `define ETH_DIRECTED_PROTOCOL_ERROR_SV
 
